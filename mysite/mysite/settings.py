b2e0c7baa3e19097e547c13576149d0158b12da8
Pittaya <ptydvp@gmail.com>
2021-05-17 23:32:27 +0700

    ใ

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,13 +26,13 @@ SECRET_KEY = 'g)to(z&px$v!yqxjthmcp09w96v$!l_8ya%xp(ahv0e!kp_77k'
 DEBUG = True
 
 #EC2-safereserve - 1
-# ALLOWED_HOSTS = ['35.153.131.216']
+ALLOWED_HOSTS = ['35.153.131.216', '10.0.1.106']
 
 #EC2-safereserve - 2
 # ALLOWED_HOSTS = ['54.147.118.129']
 
 # ELB-safereserve
-ALLOWED_HOSTS = ['safeReserve-elb-2083691580.us-east-1.elb.amazonaws.com', '54.147.118.129']
+# ALLOWED_HOSTS = ['safeReserve-elb-2083691580.us-east-1.elb.amazonaws.com', '54.147.118.129']
 
 # Application definition
 
